71d28c48baf0bd668ff008def8df038cc3a914a7
lavozdemanaureapp <alexisemch0@gmail.com>
2026-04-02 16:04:14 -0400

    Add files via upload

--- control radio/sw.js
+++ control radio/sw.js
@@ -1,11 +1,24 @@
-const cacheName = 'admin-manaure-v1';
-const staticAssets = ['./', './index.html', './manifest.json'];
+const cacheName = 'admin-manaure-v2';
+const staticAssets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo.png'
+];
 
+// Instalación: Guarda los archivos en la memoria del celular
 self.addEventListener('install', async e => {
   const cache = await caches.open(cacheName);
   await cache.addAll(staticAssets);
+  return self.skipWaiting();
 });
 
+// Activación: Limpia memorias viejas
+self.addEventListener('activate', e => {
+  self.clients.claim();
+});
+
+// Peticiones: Permite que la app abra rápido
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
